e5fd86b1bbd0048787e78e15ea8cafc68cdc6338
younghoon12 <tutu9506@gmail.com>
2024-09-09 17:51:50 +0900

    ch01

---
+++ ch01/src/main/java/com/ch01/App.java
@@ -0,0 +1,18 @@
+package com.ch01;
+
+/**
+ *  날짜 : 2024/09/02
+ *  이름 : 김영훈
+ *  내용 : 1장 Spring 개요 및 개발환경 구축 실습
+ *
+ *
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hello spring!");
+    }
+}
